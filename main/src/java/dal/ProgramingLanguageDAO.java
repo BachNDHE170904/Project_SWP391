a601149e4ff710386c44116c0ae901e5dcc0c736
leanhtu1022 <144982818+leanhtu1022@users.noreply.github.com>
2023-10-15 02:58:02 +0700

    Modify mentor create and add manage program language
    Revert "Modify mentor create and add manage program language"
    
    This reverts commit 02a6b7f8eaeee652d5a70e0d5a40504d0b91490a.

--- main/src/java/dal/ProgramingLanguageDAO.java
+++ main/src/java/dal/ProgramingLanguageDAO.java
@@ -2,15 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package dal;
+package DAL;
 
+import Model.ProgramingLanguage;
+import dal.BaseDAO;
+import dal.UserDAO;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import model.ProgramingLanguage;
 
 public class ProgramingLanguageDAO extends BaseDAO<ProgramingLanguage> {
 
@@ -19,26 +22,6 @@ public class ProgramingLanguageDAO extends BaseDAO<ProgramingLanguage> {
         try {
             String sql = "SELECT * FROM ProgrammingLanguage, LanguageStatus where ProgrammingLanguage.languageStatusId= LanguageStatus.languageStatusId \n";
             PreparedStatement statement = connection.prepareStatement(sql);
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()) {
-                ProgramingLanguage s = new ProgramingLanguage();
-                s.setLanguageId(rs.getInt("languageId"));
-                s.setLanguageName(rs.getString("languageName"));
-                s.setLanguageStatus(rs.getString("languageStatus"));
-                programingLanguages.add(s);
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return programingLanguages;
-    }
-
-    public ArrayList<ProgramingLanguage> getActiveProgramingLanguage() {
-        ArrayList<ProgramingLanguage> programingLanguages = new ArrayList<>();
-        try {
-            String sql = "SELECT * FROM ProgrammingLanguage, LanguageStatus where ProgrammingLanguage.languageStatusId= LanguageStatus.languageStatusId and LanguageStatus.languageStatus=? \n";
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1, "active");
             ResultSet rs = statement.executeQuery();
             while (rs.next()) {
                 ProgramingLanguage s = new ProgramingLanguage();
@@ -75,7 +58,7 @@ public class ProgramingLanguageDAO extends BaseDAO<ProgramingLanguage> {
     public boolean insertProgramingLanguage(ProgramingLanguage pro) {
         try {
             String sql = "insert into ProgrammingLanguage(languageName,languageStatusId) values(?,?)\n;";
-            PreparedStatement statement = connection.prepareStatement(sql);
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             int languageStatusId;
             statement.setString(1, pro.getLanguageName());
             if (pro.getLanguageStatus().equalsIgnoreCase("Active")) {
@@ -85,6 +68,7 @@ public class ProgramingLanguageDAO extends BaseDAO<ProgramingLanguage> {
             }
             statement.setInt(2, languageStatusId);
             statement.executeUpdate();
+            statement.getGeneratedKeys();
             return true;
         } catch (SQLException ex) {
             Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
